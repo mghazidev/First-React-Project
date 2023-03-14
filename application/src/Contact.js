08b2ba946248329740d66cf5d4407e01c446c098
mghazidev <mghazidev@gmail.com>
2023-03-14 18:58:50 +0500

    React

---
+++ application/src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+    return (
+      <div className='contact-page'>
+        <h1>Welcome contact</h1>
+      </div>
+    )
+  }
+  
+  export default Contact;
+  
---
+++ application/src/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Services() {
+    return (
+      <div className='services-page my-5'>
+      </div>
+    )
+  }
+  
+  export default Services;
+  
